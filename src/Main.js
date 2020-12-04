41ecf826e8f5015626aaad9632447c59636f2c31
Abhilash Nishane <abhilash.nishane6@gmail.com>
2020-12-05 00:51:34 +0530

    installed carousel library and keep awake and setup basic folders and files

---
+++ src/Main.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Image,
+  TextInput,
+  FlatList,
+  TouchableOpacity
+} from 'react-native';
+
+class Main extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hey</Text>
+      </View>
+    )
+  }
+}
+
+export default Main;
