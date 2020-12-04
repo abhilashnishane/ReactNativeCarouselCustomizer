4c2b89803a7dd8790c42413b6d586e75ed2c5672
Abhilash Nishane <abhilash.nishane6@gmail.com>
2020-12-05 01:11:03 +0530

    basic carousel implemented with dummy data

--- src/Main.js
+++ src/Main.js
@@ -11,15 +11,95 @@ import {
   FlatList,
   TouchableOpacity
 } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
 
 class Main extends Component {
+
+  state = {
+    carouselArray: [
+      {
+        img: './src/assets/ab.jpg',
+        name: 'Image Name 1',
+        desc: 'Image Description 1'
+      },
+      {
+        img: './src/assets/ab.jpg',
+        name: 'Image Name 2',
+        desc: 'Image Description 2'
+      },
+      {
+        img: './src/assets/ab.jpg',
+        name: 'Image Name 3',
+        desc: 'Image Description 3'
+      },
+      {
+        img: './src/assets/ab.jpg',
+        name: 'Image Name 4',
+        desc: 'Image Description 4'
+      }
+    ]
+  }
+
+  _renderItem = ({ item, index }) => {
+
+    return (
+      <View style={styles.slide}>
+        <Image source={{ uri: item.img }} style={styles.img} />
+        <View style={styles.imgCaptionContainer}>
+          <Text>{item.name}</Text>
+          <Text>{item.desc}</Text>
+        </View>
+      </View>
+    );
+  }
+
+
+
   render() {
+
+    const sliderWidth = 350;
+    const itemWidth = 300;
+
     return (
       <View>
-        <Text>Hey</Text>
+
+        <View style={styles.carouselContainer}>
+          <Carousel
+            containerCustomStyle={styles.carouselSelf}
+            ref={(c) => {
+              this._carousel = c;
+            }}
+            data={this.state.carouselArray}
+            renderItem={this._renderItem}
+            sliderWidth={sliderWidth}
+            itemWidth={itemWidth}
+          />
+        </View>
+
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  carouselContainer: {
+    alignItems: 'center',
+    paddingVertical: 30
+  },
+  carouselSelf: {
+    // backgroundColor: '#f5f5f5'
+  },
+  slide: {
+    backgroundColor: '#e1e1e1',
+    height: 380
+  },
+  img: {
+    width: '100%',
+    height: 300
+  },
+  imgCaptionContainer: {
+    alignItems: 'center'
+  }
+});
 
 export default Main;
